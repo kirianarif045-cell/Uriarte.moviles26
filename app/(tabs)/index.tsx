c831035d5771646cb3f0029add75af7f3a145211
Kweovu <kirianarif045@gmail.com>
2026-05-21 15:42:59 -0300

    correcion de imagen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,9 +14,10 @@ export default function App() {
         ¡Hola mundo React Nativexd!
       </Text>
       <Text style={{ fontSize: 18 }}>esto es un super texto creado por mi</Text>
-      <Image 
-        source={require("")}
-        style={{ width: 50, height: 50 }}
+      <Image
+        source={{
+          uri: "https://i.pinimg.com/1200x/5e/84/9a/5e849adab971e3d40d0dcea8c738d295.jpg"}}
+          style={{ width: 50, height: 50 }}
       />
     </View>
   );
