c0f022e71911b492d00d15400f308deea51420eb
lourdes222 <lourdes.munoz666222@gmail.com>
2026-06-04 19:33:30 -0300

    Kirian  y santos trabajo 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,64 +1,172 @@
-import { Button, Image, Text, TextInput, View } from "react-native";
+import { Button, Image, ScrollView, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
+    <ScrollView>
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
         backgroundColor: "grey",
+        paddingTop: 60,
+        alignItems: "center",
       }}
     >
       <Text
         style={{
           color: "white",
-          fontSize: 50,
+          fontSize: 40,
           fontStyle: "italic",
+          marginBottom: 10,
         }}
       >
         ¡Bienvenido a nuestra pagina!
       </Text>
 
-      <Text style={{ fontSize: 18 }}>
+      <Text
+        style={{
+          fontSize: 18,
+          marginBottom: 30,
+          color: "white",
+        }}
+      >
         Descripcion provisional improvisada
       </Text>
-      
+
       <View
         style={{
-          flex: 1,
           flexDirection: "row",
           justifyContent: "center",
-          alignItems: "center",
+          alignItems: "flex-start",
+          gap: 20, 
         }}
-      
       >
-        <Image
-          source={{
-            uri: "https://media1.tenor.com/m/wBzXX1bly3sAAAAd/subaru-natsuki-subaru.gif",
+        {/* CAJA 1 */}
+        <View
+          style={{
+            width: 320,
+            padding: 15,
+            borderRadius: 15,
+            backgroundColor: "darkgreen",
+            alignItems: "center",
+            marginTop: 160,
           }}
-          style={{ width: 300, height: 300,margin:10 }}
-        />
-        <Image
-          source={{
-            uri: "https://media1.tenor.com/m/85WyIWYK_tQAAAAd/re-zero-natsuki-subaru.gif  ",
+        >
+          <Image
+            source={{
+              uri: "https://media1.tenor.com/m/wBzXX1bly3sAAAAd/subaru-natsuki-subaru.gif",
+            }}
+            style={{
+              width: 250,
+              height: 250,
+              marginBottom: 15,
+              borderRadius: 10,
+            }}
+          />
+
+          <TextInput
+            placeholder="Escribe aqui"
+            style={{
+              width: "100%",
+              borderWidth: 1,
+              padding: 10,
+              backgroundColor: "white",
+              marginBottom: 15,
+              borderRadius: 8,
+            }}
+          />
+
+          <Button
+            title="Presionar"
+            onPress={() => alert("Boton 1 presionado")}
+            color="blue"
+          />
+        </View>
+
+        {/* CAJA 2 */}
+        <View
+          style={{
+            width: 320,
+            padding: 15,
+            borderRadius: 15,
+            backgroundColor: "darkgreen",
+            alignItems: "center",
+            marginTop: 80, 
           }}
-          style={{ width: 300, height: 300,margin:10 }}
-        />
+        >
+          <Image
+            source={{
+              uri: "https://media1.tenor.com/m/wBzXX1bly3sAAAAd/subaru-natsuki-subaru.gif",
+            }}
+            style={{
+              width: 250,
+              height: 250,
+              marginBottom: 15,
+              borderRadius: 10,
+            }}
+          />
+
+          <TextInput
+            placeholder="Escribe aqui"
+            style={{
+              width: "100%",
+              borderWidth: 1,
+              padding: 10,
+              backgroundColor: "white",
+              marginBottom: 15,
+              borderRadius: 8,
+            }}
+          />
+
+          <Button
+            title="Presionar"
+            onPress={() => alert("Boton 2 presionado")}
+            color="blue"
+          />
+        </View>
+
+        {/* CAJA 3 */}
+        <View
+          style={{
+            width: 320,
+            padding: 15,
+            borderRadius: 15,
+            backgroundColor: "darkgreen",
+            alignItems: "center",
+            marginTop: 0,
+          }}
+        >
+          <Image
+            source={{
+              uri: "https://media1.tenor.com/m/wBzXX1bly3sAAAAd/subaru-natsuki-subaru.gif",
+            }}
+            style={{
+              width: 250,
+              height: 250,
+              marginBottom: 15,
+              borderRadius: 10,
+            }}
+          />
+
+          <TextInput
+            placeholder="Escribe aqui"
+            style={{
+              width: "100%",
+              borderWidth: 1,
+              padding: 10,
+              backgroundColor: "white",
+              marginBottom: 15,
+              borderRadius: 8,
+            }}
+          />
+
+          <Button
+            title="Presionar"
+            onPress={() => alert("Boton 3 presionado")}
+            color="blue"
+          />
+        </View>
       </View>
-
-      <TextInput
-      placeholder="Escribe aqui"
-      style={{borderWidth: 1, padding: 8,backgroundColor:"white",marginBottom: 10}} 
-      />
-
-      <Button
-        title= "presionar"
-        onPress={()=>alert("Boton presionado")}
-        color="purple"
-      />
-      
     </View>
+    </ScrollView>
   );
- 
 }
